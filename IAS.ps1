b56c668480917c9549022d07871f9fe92b0a7308
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2023-01-31 12:11:04 +0600

    Update IAS.ps1

--- IAS.ps1
+++ IAS.ps1
@@ -1,9 +1,9 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/lstprjct/IDM-Activation-Script/main/IAS_0.7_ModByPiash.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/lstprjct/IDM-Activation-Script/main/IDMA-1.cmd'
 
-$FilePath = "$env:TEMP\IAS.cmd"
+$FilePath = "$env:TEMP\IDMA.cmd"
 
 try {
     Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
