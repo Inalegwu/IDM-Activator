d1a71f75a7ab49a31c8b998e894d20863bc681bf
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2023-01-31 12:12:35 +0600

    Update and rename IAS_0.7_ModByPiash.cmd to IDMA-1.cmd

--- IDMA-1.cmd
+++ IDMA-1.cmd
@@ -815,10 +815,10 @@ _________________________________
 
 Possible accepted values,
 
-"IAS_xxxxxxxx.cmd" /act
-"IAS_xxxxxxxx.cmd" /res
-"IAS_xxxxxxxx.cmd" /act /s
-"IAS_xxxxxxxx.cmd" /res /s
+"IDMA-xxxxxxxx.cmd" /act
+"IDMA-xxxxxxxx.cmd" /res
+"IDMA-xxxxxxxx.cmd" /act /s
+"IDMA-xxxxxxxx.cmd" /res /s
 
 _________________________________
 
